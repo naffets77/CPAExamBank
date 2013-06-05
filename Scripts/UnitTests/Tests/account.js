e29d52c1f6ce3ad2f9aa74e415cb1aea50ab43be
Steffan McMurrin <naffets77@gmail.com>
2013-06-05 14:49:07 -0700

    qUnit Login Test Case
    Setting up first qUnit test case for testing the login service from
    javascript.

--- Scripts/UnitTests/Tests/account.js
+++ Scripts/UnitTests/Tests/account.js
@@ -1,3 +1,22 @@
-﻿test("hello test", function () {
-    ok(1 == "1", "Passed!");
+﻿
+
+asyncTest("Login", function () {
+
+    expect(1);
+
+    var postData = {
+        email : "demo_account@cpaexambank.com",
+        password: "e368b9938746fa090d6afd3628355133",
+        service: "service_account",
+        call : "login"
+    }
+
+
+    $.post("/PHP/services.php", postData, function (data) {
+
+        ok(true, "Passed and ready to resume!");
+        start();
+
+    });
+
 });
