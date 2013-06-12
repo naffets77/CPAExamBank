f5c5c567dfb8ca1f7be13c7c03b4164ebf24a11f
Steffan McMurrin <naffets77@gmail.com>
2013-06-12 16:09:44 -0700

    Update Unit Test
    Added hash to the post request for test Update Email

--- PHP/UnitTests/account.php
+++ PHP/UnitTests/account.php
@@ -45,7 +45,12 @@ class TestAccount extends UnitTestCase {
     
         BuildResultViewer($result, "service_account :: login");
         
-        $result = simulatePostRequest(array("email"=>"updated_demo_account@cpaexambank.com"), "service_account","updateLoginEmail");
+        $result = simulatePostRequest(array("email"=>"updated_demo_account@cpaexambank.com", "hash"=> $result['hash']), "service_account","updateLoginEmail");
+    
+        BuildResultViewer($result,"service_account :: updateLoginEmail"); 
+        
+        // clean up
+        $result = simulatePostRequest(array("email"=>"demo_account@cpaexambank.com", "hash"=> $result['hash']), "service_account","updateLoginEmail");
     
         BuildResultViewer($result,"service_account :: updateLoginEmail"); 
           
