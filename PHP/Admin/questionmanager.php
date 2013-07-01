f761b986da299971ec4c5efab7fddd482ce8d2d2
Steffan McMurrin <steffan77@gmail.com>
2013-06-30 21:21:46 -0700

    Question Manager UI Update

--- PHP/Admin/questionmanager.php
+++ PHP/Admin/questionmanager.php
@@ -8,6 +8,7 @@
 
 <html>
     <body>
+        <br /><br />
         <p>
             For best results don't select any filters when searching for a specific question!
         </p>
@@ -15,25 +16,25 @@
         <table>
             <tr>
                 <td>
-                    To find a specific question, search by...
-                </td>
-                <td>
+                    To find a specific question, search by...<br />
                     <select>
                         <option>Select One (Optional)</option>
                         <option>Question Reference ID</option>
                         <option>Question Text</option>
                     <select>
+                    
+                    <input type='text' />
                 </td>
             </tr>
             
             <tr>
-                <td colspan='2'>
+                <td colspan='2' style='padding-top:10px;'>
                     Filter Questions...
                 </td>
             </tr>
             <tr>
-                <td>Approved</td>
                 <td>
+                    Approved<br />
                     <select>
                         <option>Select To Apply Filter</option>
                         <option>No</option>
@@ -41,6 +42,11 @@
                         <option>Both</option>
                     </select>
                 </td>
+            </tr>
+            <tr>
+            <td>
+                <input type='submit' value='Search' />
+            </td>
             </tr>
         </table>
     
