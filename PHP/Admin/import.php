c624b5d34cbe6802ef9a337510186ab0dd7d4e85
Steffan McMurrin <steffan77@gmail.com>
2013-06-30 19:48:02 -0700

    Questions Upload Script

--- PHP/Admin/import.php
+++ PHP/Admin/import.php
@@ -13,7 +13,7 @@ if(!isset($_POST['submit'])){
 <html>
     <body>
 
-    <form action="<?php echo $_SERVER;?>" method="post" enctype="multipart/form-data">
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" enctype="multipart/form-data">
         <label for="file">Filename:</label>
         <input type="file" name="file" id="file"><br>
         <input type="submit" name="submit" value="Submit">
