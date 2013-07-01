391c96b79c06df8671a875a2bc58da9d742bb567
Steffan McMurrin <steffan77@gmail.com>
2013-06-30 18:31:46 -0700

    Questions Upload Script

--- PHP/Admin/import.php
+++ PHP/Admin/import.php
@@ -6,7 +6,7 @@
 
 
 <?php
-if(!isset($_Post['submit'])){
+if(!isset($_POST['submit'])){
 ?>
 <html>
     <body>
@@ -33,10 +33,28 @@ else{
     }
     else
     {
+        echo "<h3>File Data </h3>";
         echo "Upload: " . $_FILES["file"]["name"] . "<br>";
         echo "Type: " . $_FILES["file"]["type"] . "<br>";
         echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
         echo "Stored in: " . $_FILES["file"]["tmp_name"];
+        
+        echo "<h3> Reading File </h3>";
+        
+        $row = 1;
+        if (($handle = fopen($_FILES['uploadedfile']['tmp_name'], "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $num = count($data);
+                echo "<p> $num fields in line $row: <br /></p>\n";
+                $row++;
+                for ($c=0; $c < $num; $c++) {
+                    echo $data[$c] . "<br />\n";
+                }
+            }
+            fclose($handle);
+        }
+        
+        
     } 
    
 
