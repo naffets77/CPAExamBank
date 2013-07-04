12290e4a2ca3faa4c911ad6a702b82121093da67
Steffan McMurrin <steffan77@gmail.com>
2013-07-03 22:58:45 -0700

    Updating registration unit test

--- PHP/UnitTests/account.php
+++ PHP/UnitTests/account.php
@@ -9,10 +9,13 @@ class TestAccount extends UnitTestCase {
 
     function testRegistration(){
  
-        BuildTestHeader("Register New User", "service_account", "registerNewUser", "Test registering a new user", null, null);
+    
+        $email = "registerTest_" . rand(5,25000) ."@cpaexambank.com";
+    
+        BuildTestHeader("Register New User", "service_account", "registerNewUser", "Test registering a new user - $email", null, null);
   
         $sections = "[{'FAR': '0','AUD': '1','BEC': '0','REG': '1'}]";
-        $email = "registerTest_" . rand() ."@cpaexambank.com";
+
   
         $result = simulatePostRequest(array("email"=>$email, "password"=>"e368b9938746fa090d6afd3628355133","sections"=>$sections), "service_account","registerNewUser");
     
