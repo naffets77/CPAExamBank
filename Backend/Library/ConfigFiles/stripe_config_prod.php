25c3a935d5698478ddfa2cfd9f7c2a93aff83a35
Marcus Rico <marcusrico32@gmail.com>
2013-10-21 21:19:58 -0700

    Testing git commit from PHP storm.

--- Backend/Library/ConfigFiles/stripe_config_prod.php
+++ Backend/Library/ConfigFiles/stripe_config_prod.php
@@ -4,8 +4,8 @@ require_once('/srv/lib/stripe-php/Stripe.php');
 class stripe_configuration{
 
     //TODO: 7/9/2013 replace with real keys when ready
-    public static $secret_key = "sk_test_mkGsLqEW6SLnZa487HYfJVLf";
-    public static $public_key = "pk_test_czwzkTp2tactuLOEOqbMTRzG";
+    public static $secret_key = "sk_test_mkGsLqEW6SLnZa487HYfJVLf"; //sk_live_UIcybit30Z3LxXB7KpbRAmtS
+    public static $public_key = "pk_test_czwzkTp2tactuLOEOqbMTRzG"; //pk_live_PpVwQkuyOIGmuxmp9v4KZqtv
 
     function stripe_configuration(){
         Stripe::setApiKey(stripe_configuration::$secret_key);
