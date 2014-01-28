bde83877b6efed15285ca7f857f0de263124f062
Steffan McMurrin <steffan77@gmail.com>
2014-01-27 21:20:21 -0800

    Added Reporting Page

---
+++ PHP/Reporting/reports.php
@@ -0,0 +1,35 @@
+﻿<?php
+
+ // Do check to verify an admin user is logged in
+ var_dump($_POST);
+
+
+?>
+
+
+
+<html>
+	<head>
+	</head>
+	<body>
+
+
+		<form  method="post" action="<?php echo $PHP_SELF;?>">
+			<table>
+				<tr>
+					<td>
+						<select name='report'>
+							<option value='rep-registered_users'>Registered Users</option>
+						</select>
+					</td>
+					<td>
+						<input type='submit' />
+					</td>
+				</tr>
+			</table>
+		</form>
+
+
+
+	</body>
+</html>
