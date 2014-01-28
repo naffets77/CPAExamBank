b7f1215d3090c358fd566df212bb9ca23d8f7d04
Steffan McMurrin <steffan77@gmail.com>
2014-01-27 21:36:49 -0800

    Setting up DB Connection

--- PHP/Reporting/ReportBuilder/reports.php
+++ PHP/Reporting/ReportBuilder/reports.php
@@ -1,13 +1,31 @@
 ﻿<?php
 
+
+
+
 class ReportBuilder {
     
+
+
 
 
     public function build_report($report) {
 
 		echo "Building Report: ". $report;
 
+		$this->connect_db();
+
+
     }
+
+	public function connect_db(){
+
+		mysql_connect("198.211.105.160", "root", "Naffets77") or die(mysql_error());
+		mysql_select_db("ciborium_prod") or die(mysql_error());
+	
+	}
+
 }
+
+
 ?>
