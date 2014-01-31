31a2e98f85296cc4a64cd99ce30eba03511e9b35
Steffan McMurrin <steffan77@gmail.com>
2014-01-30 21:10:54 -0800

    example report graph

--- PHP/Reporting/reports.php
+++ PHP/Reporting/reports.php
@@ -17,6 +17,9 @@ if(isset($_POST['report'])){
 <html>
 	<head>
 
+		<link rel="stylesheet" type="text/css" href="css/visualize.css">
+		<link rel="stylesheet" type="text/css" href="css/visualize-light.css">
+
 		<style>
 			table.db-table 		{ border-right:1px solid #ccc; border-bottom:1px solid #ccc; }
 			table.db-table td	{ padding:5px; border-left:1px solid #ccc; border-top:1px solid #ccc; }
@@ -51,9 +54,77 @@ if(isset($_POST['report'])){
 		?>
 
 
+		<table>
+			<caption>2009 Employee Sales by Department</caption>
+			<thead>
+				<tr>
+					<td></td>
+					<th scope="col">food</th>
+					<th scope="col">auto</th>
+					<th scope="col">household</th>
+					<th scope="col">furniture</th>
+					<th scope="col">kitchen</th>
+					<th scope="col">bath</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th scope="row">Mary</th>
+					<td>190</td>
+					<td>160</td>
+					<td>40</td>
+					<td>120</td>
+					<td>30</td>
+					<td>70</td>
+				</tr>
+				<tr>
+					<th scope="row">Tom</th>
+					<td>3</td>
+					<td>40</td>
+					<td>30</td>
+					<td>45</td>
+					<td>35</td>
+					<td>49</td>
+				</tr>
+				<tr>
+					<th scope="row">Brad</th>
+					<td>10</td>
+					<td>180</td>
+					<td>10</td>
+					<td>85</td>
+					<td>25</td>
+					<td>79</td>
+				</tr>
+				<tr>
+					<th scope="row">Kate</th>
+					<td>40</td>
+					<td>80</td>
+					<td>90</td>
+					<td>25</td>
+					<td>15</td>
+					<td>119</td>
+				</tr>		
+			</tbody>
+		</table>	
+
+
 
 		<script src='../../Scripts/jquery-2.0.3.min.js' type='text/javascript'></script>
 		<script src='jQuery.Vis/js/visualize.jQuery.js' type='text/javascript'></script>
+
+		<script type='text/javascript'>
+			$(document).on('ready', function(){
+		
+				$(function(){
+					$('table').visualize({type: 'pie', height: '300px', width: '420px'});
+					$('table').visualize({type: 'bar', width: '420px'});
+					$('table').visualize({type: 'area', width: '420px'});
+					$('table').visualize({type: 'line', width: '420px'});
+				});
+
+
+			});
+		</script>
 
 	</body>
 </html>
